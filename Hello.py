22fc095793a0215e2bf84191d44cbc8dba223a32
Adnan Sheik <143235539+adnansheik137@users.noreply.github.com>
2024-04-12 00:10:37 -0700

    removed tamil
    library was invalid

--- Hello.py
+++ Hello.py
@@ -8,7 +8,7 @@ available_packages = argostranslate.package.get_available_packages()
 
 
 st.write("Translator app")
-st.write("You can translate into these languages: Chinese, Spanish, German, and French")
+st.write("You can translate into these languages: Chinese, Spanish, German, French, and Arabic")
 
 st.write("Enter text and choose your language:")
 
@@ -16,7 +16,7 @@ text = st.text_input("Enter input:", "")
 
 #methods
 #TASK 1
-cs = ["Chinese", "Spanish", "German", "French", "Arabic", "Tamil"]#add options for spanish, german and french. You need to edit the cs list.
+cs = ["Chinese", "Spanish", "German", "French", "Arabic"]#add options for spanish, german and french. You need to edit the cs list.
 #after you add options the sidebar would show 4 languages
 classification_space = st.sidebar.selectbox("Language to be translated into:", cs)
 option = ''
@@ -38,7 +38,6 @@ language_codes = {
     "German": "de",
     "French": "fr",
     "Arabic": "ar",
-    "Tamil": "ta"
 }
 
 option = language_codes.get(classification_space, '')
